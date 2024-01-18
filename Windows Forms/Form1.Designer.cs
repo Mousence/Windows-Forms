dfaaf47a48919b0e8218570610983a8d63d32a3e
79141 <79141@DESKTOP-GMGD17F>
2024-01-18 19:00:40 +0900

    Commands in tray

--- Windows Forms/Form1.Designer.cs
+++ Windows Forms/Form1.Designer.cs
@@ -41,17 +41,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 42);
+            this.label1.Size = new System.Drawing.Size(308, 42);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.MouseHover += new System.EventHandler(this.label1_MouseHover);
+            this.label1.Text = "Most Great Alarm";
+            this.label1.DoubleClick += new System.EventHandler(this.label1_MouseDoubleClick);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label2
@@ -60,20 +62,18 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(12, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(118, 42);
+            this.label2.Size = new System.Drawing.Size(0, 42);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
             // 
             // cbShowDate
             // 
-            this.cbShowDate.AutoSize = true;
-            this.cbShowDate.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.cbShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.cbShowDate.Location = new System.Drawing.Point(19, 120);
             this.cbShowDate.Name = "cbShowDate";
             this.cbShowDate.Size = new System.Drawing.Size(138, 30);
             this.cbShowDate.TabIndex = 2;
             this.cbShowDate.Text = "Show date";
+            this.cbShowDate.Click += new System.EventHandler(this.cbShowDate_Click);
             // 
             // btnHideControls
             // 
@@ -94,13 +94,13 @@
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
-            this.btnExit.Click += new System.EventHandler(this.btnExit_Click_1);
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(519, 450);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnHideControls);
             this.Controls.Add(this.cbShowDate);
@@ -110,6 +110,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
+            this.TopMost = true;
             this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
             this.PerformLayout();
--- Windows Forms/Form1.cs
+++ Windows Forms/Form1.cs
@@ -8,19 +8,37 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Windows_Forms.Properties;
 
 namespace Windows_Forms
 {
     public partial class Form1 : Form
     {
+        private NotifyIcon trayIcon;
+
         public Form1()
         {
             InitializeComponent();
-            label1.Width = this.Width - 10;
-            label2.Width = this.Width - 10;
+            this.StartPosition = FormStartPosition.Manual;
+            int startX = Screen.PrimaryScreen.Bounds.Width - this.Width ;
+            int startY = 25;
+            this.SetDesktopLocation(startX, startY);
+            ControlsVisibility(false);
+
+            trayIcon = new NotifyIcon()
+            {
+                Icon = new Icon("C:\\Users\\79141\\source\\repos\\WindowsForms\\Windows Forms\\Clock.ico"),
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Show controls", label1_MouseDoubleClick),
+                new MenuItem("Show date", cbShowDate_Click),
+                new MenuItem("Exit", btnExit_Click)
+            }),
+                Visible = true
+            };
+
         }
         void ControlsVisibility(bool visible) {
-            cbShowDate.Checked = visible;
+            cbShowDate.Visible = visible;
             btnExit.Visible = visible;
             btnHideControls.Visible = visible;
             this.ShowInTaskbar = visible;
@@ -31,22 +49,28 @@ namespace Windows_Forms
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Text = DateTime.Now.ToString("hh:mm:ss tt"); //tt - 24 hour format
-            label2.Text = DateTime.Now.ToString("yyyy.MM.dd ddd"); //ddd - Day of the week
-            label2.Visible = cbShowDate.Checked;
+            //label2.Text = DateTime.Now.ToString("yyyy.MM.dd ddd"); //ddd - Day of the week
+            if (cbShowDate.Checked) {
+                string date = DateTime.Now.ToString("yyyy.MM.dd ddd");
+                label1.Text = $"{label1.Text}\n{date}";
+            }
         }
-        private void label1_MouseHover(object sender, EventArgs e)
+        private void label1_MouseDoubleClick(object sender, EventArgs e)
         {
             ControlsVisibility(true);
         }
-
         private void btnHideControls_Click_1(object sender, EventArgs e)
         {
             ControlsVisibility(false);
         }
-
-        private void btnExit_Click_1(object sender, EventArgs e)
+        private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        /// 
+        private void cbShowDate_Click(object sender, EventArgs e)
+        {
+            ControlsVisibility(!cbShowDate.Checked);
         }
     }
 }
